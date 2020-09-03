a5ad1aedfc74f99d353cf7eb04c91a30090aa248
pedro.rodrigues1 <pedro.rodrigues@semantix.com.br>
2020-09-03 20:50:06 -0300

    Ajustando whitenoise no projeto

--- djangoecommerce/urls.py
+++ djangoecommerce/urls.py
@@ -21,7 +21,6 @@ from core import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^contato/$', views.contact, name='contact'),
-    url(r'^produto$', views.product, name='product'),
-    url(r'^produtos/', include('catalog.urls', namespace='catalog')),
+    url(r'^catalogo/', include('catalog.urls', namespace='catalog')),
     url(r'^admin/', admin.site.urls),
 ]
